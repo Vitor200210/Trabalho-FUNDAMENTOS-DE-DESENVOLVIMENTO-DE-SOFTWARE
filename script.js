94c187e021bf21fafff9b1f1a44f23cdfabff2cd
Vitor Dias <vitordiasgwgesep@gmail.com>
2025-05-19 21:31:20 -0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+const botao = document.getElementById('botao');
+    const mensagemDiv = document.createElement('div');
+    mensagemDiv.id = 'mensagem-motivacional';
+    
+    botao.insertAdjacentElement('afterend', mensagemDiv);
+
+    const mensagens = [
+        "O sucesso começa com a vontade de tentar.",
+    ];
+
+    botao.addEventListener('click', function() {
+        
+        mensagemDiv.textContent = mensagemDiv;
+        mensagemDiv.style.display = 'block';
+    })
